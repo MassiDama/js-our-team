6d95ae39627e4d29576192894b0210cfc31e4391
Massidama <massidama@outlook.it>
2023-12-01 15:26:50 +0100

    feat: created info array

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const teamMembers = [
+    
+    {
+        "nome": "Wayne",
+        "cognome": "Barnett",
+        "ruolo": "Founder & Ceo",
+        "image": "wayne-barnett-founder-ceo.jpg"
+    }
+
+    {
+        "nome": "Angela",
+        "cognome": "Carrol",
+        "ruolo": "Chief Editor",
+        "image": "angela-carrol-chief-editor.jpg"
+    }
+
+    {
+        "nome": "Walter",
+        "cognome": "Gordon",
+        "ruolo": "Office Manager",
+        "image": "walter-gordon-office-manager.jpg"
+    }
+
+    {
+        "nome": "Angela",
+        "cognome": "Lopez",
+        "ruolo": "Social Media Manager",
+        "image": "angela-lopez-social-media-manager.jpg"
+    }
+
+    {
+        "nome": "Scott",
+        "cognome": "Estrada",
+        "ruolo": "Developer",
+        "image": "scott-estrada-developer.jpg"
+    }
+
+    {
+        "nome": "Barbara",
+        "cognome": "Ramos",
+        "ruolo": "Graphic Designer",
+        "image": "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+
+
+
+
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sotto forma di stringhe
